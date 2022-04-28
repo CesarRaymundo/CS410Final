7c318c88f7d7b78b816b9447d9c635a9fbfff6d9
CesarRaymundo <raymundocesar95@gmail.com>
2022-04-28 16:50:58 -0600

    Yuhhh

--- src/Project.java
+++ src/Project.java
@@ -56,9 +56,11 @@ public static void createClass(String class_course, String class_name, String cl
 		stmt.setString(2, latest_class_term);
 		ResultSet rs = stmt.executeQuery();
 		if (rs.next()) {
-			System.out.println(rs.getInt("class_id")+", "+ rs.getString("class_course") + ", " + rs.getString("class_name") + ", " + rs.getString("class_term") + ", " + rs.getString("class_section"));
-		 if(rs.next()) {
-			 System.out.println("**WARNING** there are multiple sections of the same class");
+		 if(!rs.next()) {
+			 System.out.println(rs.getInt("class_id")+", "+ rs.getString("class_course") + ", " + rs.getString("class_name") + ", " + rs.getString("class_term") + ", " + rs.getString("class_section"));
+		 }
+		 else {
+			 System.out.println("Multiple sections of the same course found");
 		 }
 		}
 	}catch (SQLException e) {
@@ -74,11 +76,13 @@ public static void createClass(String class_course, String class_name, String cl
 			stmt.setString(1, class_course);
 			stmt.setString(2, class_term);
 			ResultSet rs = stmt.executeQuery();
-			if(rs.next()) {
-				System.out.println(rs.getInt("class_id")+", "+ rs.getString("class_course") + ", " + rs.getString("class_name") + ", " + rs.getString("class_term") + ", " + rs.getString("class_section"));
-			if(rs.next()) {
-				System.out.println("**WARNING** there are multiple sections of the same class");
-			}
+			if (rs.next()) {
+				if(!rs.next()) {
+					System.out.println(rs.getInt("class_id")+", "+ rs.getString("class_course") + ", " + rs.getString("class_name") + ", " + rs.getString("class_term") + ", " + rs.getString("class_section"));
+				}
+				else {
+					System.out.println("Multiple sections of the same course found");
+				}
 			}
 		}catch (SQLException e) {
 			e.printStackTrace();
